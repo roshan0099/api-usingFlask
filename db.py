cf10cbdf6c16fe444ba22b51122df2f00752b724
roshan0099 <roshan09ka@gmail.com>
2021-01-13 15:03:31 +0530

    commented and added a function
    added a new function for random call queries

--- db.py
+++ db.py
@@ -4,14 +4,26 @@ class Model():
 	
 	def __init__(self) :
 
-		self.client = MongoClient(os.environ.get('KEY'))
+		self.client = MongoClient("mongodb+srv://roshan:mrk0099mrk@cluster0.al8qd.mongodb.net/checking?retryWrites=true&w=majority")
+		# self.client = MongoClient(os.environ.get('KEY'))
 		self.db = self.client["checking"]
 		self.collection = self.db["check"]
+
+	#function to find a particular movie 	
 	def finding(self,name = "Psycho") :
-		data = []
 		info = self.collection.find({"title":name},{"_id" : False})
 		return list(info)
+
+	#function to insert movie details 	
 	def inserting(self,title,plot,rate) :
 		pass				
 	
-	
+	#funtion to fetch random movies from the database
+	def random_movies(self):
+		random_movies_list = self.collection.aggregate([{"$sample": {"size" : 1}}])
+
+		redefined_list = list(random_movies_list)
+
+		#removing the ObjectId from the response so as to avoid the "json" error 
+		redefined_list[0].pop('_id')
+		return(redefined_list)
